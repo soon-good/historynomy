f611e73f63bba735f5289b2f4571d316d09ca635
kyle.sgjung <playmymind@daum.net>
2021-03-26 23:55:35 +0900

    Reactive Mongo Repository 세팅

---
+++ service-api/src/main/java/io/historynomy/serviceapi/marketindex/kospi/KospiController.java
@@ -0,0 +1,23 @@
+package io.historynomy.serviceapi.marketindex.kospi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class KospiController {
+
+	private KospiRepository kospiRepository;
+
+	public KospiController(KospiRepository kospiRepository){
+		this.kospiRepository = kospiRepository;
+	}
+
+	@GetMapping("/kospi/test")
+	public Mono<Kospi> kospiTest(){
+		Kospi k = new Kospi("코스피", 3500D);
+
+		Mono<Kospi> dat = kospiRepository.insert(k);
+		return dat;
+	}
+}
